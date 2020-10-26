555ae95d7c41302ed5edcc732d5e4072e6bb67c1
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-26 14:52:30 +0100

    Fixed typo in docs

--- kaskadi-authenticator.js
+++ kaskadi-authenticator.js
@@ -9,7 +9,7 @@ import { KaskadiElement, html } from 'https://cdn.klimapartner.net/modules/@kask
  * @module kaskadi-authenticator
  *
  * @param {string} lang - element's language
- * @param {Event} onlogin - after attempting to log in the event will dispatch a `login` event containing in its `detail` the response payload
+ * @param {Event} onlogin - after attempting to log in the element will dispatch a `login` event containing in its `detail` the response payload
  *
  * @example
  *
